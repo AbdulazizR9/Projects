fffa17f083e2717b18ed4064ed8ec0a6321a8f03
Abdulaziz R <razabdulaziz77@gmail.com>
2026-03-02 20:51:20 +0500

    Initial

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+overall, i have a goal to build an telegramm bot, which
+will help work in cs ecosystem, kinda it will be linked with ai
+and its goal to help analyze current academic situation and prepare
+materials or maybe some source and etc for students, it is kinda assistant
+which for education. It might seem not well at first, and thoughts like why
+not use already available ai or some bots in telegramm, but firstly
+I wanna review my python, and also begin to learn ai, and machine learning
+and i think i can just do as pet-project at least, and it will be open-source tool
+
+"""
